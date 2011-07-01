9d75f48ca187d34f83b7741bcbeb74cbf4332127
Christian Ledermann <christian.ledermann@gmail.com>
2011-07-01 15:07:56 +0000

    enable versioning

--- iwlearn/contacts/setuphandlers.py
+++ iwlearn/contacts/setuphandlers.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 import logging
 from Products.CMFCore.utils import getToolByName
+from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
+
 # The profile id of your package:
 PROFILE_ID = 'profile-iwlearn.contacts:default'
+TYPES_TO_VERSION = ('ContactPerson','ContactOrganization', 'ContactGroup')
+
+def setVersionedTypes(context, logger=None):
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('iwlearn.contacts')
+    portal_repository = getToolByName(context, 'portal_repository')
+    versionable_types = list(portal_repository.getVersionableContentTypes())
+    for type_id in TYPES_TO_VERSION:
+        if type_id not in versionable_types:
+            # use append() to make sure we don't overwrite any
+            # content-types which may already be under version control
+            logger.info('Adding %s to versionable types' % type_id)
+            versionable_types.append(type_id)
+            # Add default versioning policies to the versioned type
+            for policy_id in DEFAULT_POLICIES:
+                portal_repository.addPolicyForContentType(type_id, policy_id)
+    portal_repository.setVersionableContentTypes(versionable_types)
 
 def reindex_contacts(context, logger=None):
     if logger is None:
