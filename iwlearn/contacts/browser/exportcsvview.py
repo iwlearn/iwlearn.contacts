cca1cb3505772afe40d2d11f146dc37e1cbae607
Christian Ledermann <christian.ledermann@gmail.com>
2013-09-20 12:10:53 +0000

    csv export of all contact persons in a contactdb

--- iwlearn/contacts/browser/exportcsvview.py
+++ iwlearn/contacts/browser/exportcsvview.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+import logging
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+import csv
+
 from zope.interface import implements, Interface
 
 from Products.Five import BrowserView
@@ -5,6 +13,7 @@ from Products.CMFCore.utils import getToolByName
 
 from iwlearn.contacts import contactsMessageFactory as _
 
+logger = logging.getLogger(__name__)
 
 class IExportCSVView(Interface):
     """
@@ -38,33 +47,50 @@ class ExportCSVView(BrowserView):
 
 class ExportPersonsCSVView(ExportCSVView):
 
-        CSV_FIELDS =['salutation', 'firstname', 'lastname', 'jobtitle',
+    CSV_FIELDS =['salutation', 'firstname', 'lastname', 'jobtitle',
+        'address', 'po_box', 'city', 'misc', 'zipcode', 'country',
+        'email', 'remote_url', 'phone', 'fax', 'body',
          'organization', 'department', 'projectids', 'projecttitles',]
 
 
-        def __call__(self):
+    def __call__(self):
         """
         return a CSV Representation of the data in the projectdb
         """
         output = StringIO()
         fieldnames = self.CSV_FIELDS
         writer = csv.DictWriter(output, fieldnames)
-        form = self.request.form
-        query = get_query(form)
-        brains = self.portal_catalog(**query)
+        brains = self.portal_catalog(portal_type='ContactPerson', portal_state='published')
         pd = {}
         for field in fieldnames:
             pd[field] = field
         writer.writerow(pd)
-
         for brain in brains:
             pd = {}
+            logger.debug(brain.Title)
             obj = brain.getObject()
             pd['salutation']= obj.getSalutation()
             pd['firstname']= obj.getFirstname()
             pd['lastname']= obj.getLastname()
             pd['jobtitle']= obj.getJobtitle()
-            pd['organization']= obj.getOrganization().Title()
+            pd['address']= obj.getAddress()
+            pd['po_box']= obj.getPo_box()
+            pd['city']= obj.getCity()
+            pd['misc']= obj.getMisc()
+            pd['zipcode']= obj.getZipcode()
+            if obj.getCountry():
+                pd['country']= obj.getCountry()[0]
+            else:
+                pd['country']= ''
+            pd['email']= obj.getEmail()
+            pd['remote_url']= obj.getRemoteUrl()
+            pd['phone']= obj.getPhone()
+            pd['fax']= obj.getFax()
+            pd['body']= obj.getBody()
+            if obj.getOrganization():
+                pd['organization']= obj.getOrganization().Title()
+            else:
+                pd['organization'] = ''
             pd['department']= obj.getDepartment()
             projectids = []
             projecttitles = []
